6915c974094d92a176c5d4a56d9b1987d34ff515
AlexBrown4020 <balex3497@gmail.com>
2022-05-16 21:36:26 +0900

    Fix: Change env variable to fix connection issue

--- knexfile.js
+++ knexfile.js
@@ -7,31 +7,23 @@ require('dotenv').config(
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
+ module.exports = {
   development: {
-    client: 'pg',
-    connection: process.env.DB_URL,
+    client: "pg",
+    connection: process.env.DB_URL || 
+      {
+        host: process.env.DB_HOST || "127.0.0.1",
+        port: process.env.DB_PORT || 5432,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      },
+    searchPath: "public",
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
-    seeds: {directory: './data/seeds'}
-  },
-
-  testing: {
-    client: 'pg',
-    connection: process.env.DB_URL,
-    migrations: {
-      directory: './data/migrations',
+    seeds: {
+      directory: "./data/seeds",
     },
-    seeds: {directory: './data/seeds'}
-  },
-
-  production: {
-    client: 'pg',
-    connection: process.env.DB_URL,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {directory: './data/seeds'}
   }
 };
--- server/knex.js
+++ server/knex.js
@@ -1,12 +1,6 @@
-const knex = require('knex')({
-    client: 'pg',
-    connection: process.env.DB_URL || {
-        host: process.env.DB_HOST || '127.0.0.1',
-        port: process.env.DB_PORT || 5432,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    }
-});
+const config = require('./knexfile');
+const knex = require('knex');
 
-module.exports = knex;
+const db = knex(config.development);
+
+module.exports = db;
