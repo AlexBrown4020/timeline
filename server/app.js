7636416f7447d463b90af91457215254ead93e95
AlexBrown4020 <balex3497@gmail.com>
2022-05-17 21:24:51 +0900

    Feat: Implement apollo dependencies and correct app/index issues

--- server/app.js
+++ server/app.js
@@ -1,12 +1,30 @@
-const pg = require('knex')({
-    client: 'pg',
-    connection: process.env.DB_URL,
-    searchPath: ['knex', 'public'],
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const db = require('./knex');
+
+const app = express();
+
+app.use(
+    morgan(
+        ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
+    )
+);
+
+app.use(express.static(path.resolve(_dirname, '..', 'dist')));
+
+app.get('/api/events', async (req, res) => {
+    try {
+        const events = await db.select().table('events');
+        res.json(events);
+    } catch (err) {
+        console.error('Error loading events', err);
+        res.sendStatus(500);
+    }
 });
 
-const knex = require('knex')({
-    client: 'sqlite3',
-    connection: {
-        filename: ''
-    }
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, '..', 'dist', 'index.html'))
+});
+
+module.exports = app;
