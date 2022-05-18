46d91b3f5d5881e2fb349c6cba8fc0382e93e690
AlexBrown4020 <balex3497@gmail.com>
2022-05-18 15:41:46 +0900

    Fix: Attempt to solve migration issue

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,7 @@ require('dotenv').config(
         database: process.env.DB_NAME,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
+        client: "pg"
       },
     searchPath: "public",
     migrations: {
