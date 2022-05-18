03aa8b82713a5892df5e2801030f839fc9065823
AlexBrown4020 <balex3497@gmail.com>
2022-05-18 21:54:50 +0900

    Feat: Implement Events.vue

---
+++ src/components/Events.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="event">
+      <label for="events"> Title</label>
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'EventsCard',
+}
+</script>
+
+<style>
+
+</style>
