aee5c827076a7b8bef02feb5e1c4bf20b0b6dd5d
AlexBrown4020 <balex3497@gmail.com>
2022-05-18 19:51:37 +0900

    Fix: Attempt at heroku db migrations

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,9 @@ require('dotenv').config({path: './.env.local'});
  */
  module.exports = {
     client: "pg",
-    connection: process.env.DB_URL || 
+    connection: process.env.DATABASE_URL || 
       {
-        host: process.env.DB_HOST || "127.0.0.1",
+        host: process.env.POSTGRES_DB_URL || "127.0.0.1",
         port: process.env.DB_PORT || 5432,
         database: process.env.DB_NAME,
         user: process.env.DB_USER,
